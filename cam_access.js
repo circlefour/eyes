af2300185d7a585a815e598ba6466823cddc4a2d
circlefour <0100.circle@gmail.com>
2025-03-31 22:40:59 -0400

    mediapipe vision stuff loading

--- cam_access.js
+++ cam_access.js
@@ -1,17 +1,43 @@
-const constraints = {
-    audio: false,
-    video: true,
-};
+import vision from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.3";
+const { FaceLandmarker, FilesetResolver, DrawingUtils } = vision;
 
-navigator.mediaDevices
-    .getUserMedia(constraints)
-    .then((stream) => {
-        const video = document.getElementById("video");
-        video.srcObject = stream;
-        video.onloadedmetadata = () => {
-            video.play();
-        };
-    })
-    .catch((err) => {
-        console.error(`${err.name}: ${err.message}`);
+let faceLandmarker;
+let webcamRunning = false;
+let runningMode = "VIDEO";
+
+async function createFaceLandmarker() {
+    const filesetResolver = await FilesetResolver.forVisionTasks(
+        "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.3/wasm"
+    );
+    faceLandmarker = await FaceLandmarker.createFromOptions(filesetResolver, {
+        baseOptions: {
+            modelAssetPath: `https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task`,
+            delegate: "GPU"
+        },
+        outputFaceBlendshapes: true,
+        runningMode,
+        numFaces: 1
     });
+}
+createFaceLandmarker();
+
+function enableCam() {
+    const constraints = {
+        audio: false,
+        video: true,
+    };
+    
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then((stream) => {
+            const video = document.getElementById("video");
+            video.srcObject = stream;
+            video.onloadedmetadata = () => {
+                video.play();
+            };
+        })
+        .catch((err) => {
+            console.error(`${err.name}: ${err.message}`);
+        });
+}
+enableCam();
